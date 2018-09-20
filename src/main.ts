8a0b5751d28d13b4c63ecda0e64ec298f1c37ef3
Carter Hinsley <kvazau@protonmail.com>
2018-09-19 23:45:20 -0400

    Add command prompt loop
    Simple command prompt loop placeholder that doesn't really do anything
    yet.

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,37 @@
  * License: MIT
  */
 
+const promptjs = require("prompt");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 // Enter your bot token here.
-const bot_token: string = null;
+const bot_token: string = "NDg4NDI0MDI2NTkxNTkyNDYx.DoSdlA.YHbHxZiBPrgJOT6rjcJ6Fq_YG_U";
+
+function command_prompt_loop(): void
+{
+    promptjs.start();
+
+    function run_command(): void
+    {
+        promptjs.get("command", (err, result) => {
+            if (err) {
+                console.log(); // Print empty line.
+                process.exit(); // Exit program.
+            } else {
+                console.log(result.command);
+                run_command();
+            }
+        });
+    }
+    run_command();
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    command_prompt_loop();
 });
 
 client.on("message", (msg) => {
