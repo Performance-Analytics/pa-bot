c9f6af030d0ace3a70981c867facc43ce86cc82f
Carter Hinsley <kvazau@protonmail.com>
2018-09-27 23:06:47 -0400

    Add logging

--- pa-bot/main.py
+++ pa-bot/main.py
@@ -7,6 +7,7 @@ Entry point to bot.
 
 from typing import Optional
 import math
+import logging
 
 from discord.ext import commands
 from performance_utils.formulas import Formula, Brzycki, Epley, McGlothin
@@ -16,6 +17,12 @@ import performance_utils.datatypes as T
 
 # Add bot key here.
 bot_key: Optional[str] = None
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix=",", description="Performance Analytics bot")
 
